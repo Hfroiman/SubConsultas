83732b3db5ac8b93a2d659f974ee1f93b23210f6
Hfroiman <hernan.froiman@alumnos.frgp.utn.edu.ar>
2023-04-29 17:07:15 -0300

    Se sube la primera parte de las consultas realizadas.

--- Subconsultas.sql
+++ Subconsultas.sql
@@ -3,16 +3,47 @@
 -- Consulta
 -- 1
 -- La patente, apellidos y nombres del agente que labró la multa y monto de aquellas multas que superan el monto promedio.
+
+SELECT m.Patente, a.Apellidos, a.Nombres, m.Monto FROM Agentes a INNER JOIN Multas m on a.IdAgente=m.IdAgente WHERE m.Monto> (SELECT AVG(m.Monto) FROM Multas m)
+
+SELECT AVG(m.Monto) FROM Multas m ---Promedio monto
+
 -- 2
 -- Las multas que sean más costosas que la multa más costosa por 'No respetar señal de stop'.
+
+SELECT * FROM Multas m WHERE m.Monto>(SELECT top 1 m.Monto FROM Multas m INNER JOIN TipoInfracciones ti on m.IdTipoInfraccion=ti.IdTipoInfraccion WHERE ti.Descripcion='No respetar señal de stop' ORDER BY m.Monto desc)
+
+SELECT top 1 m.Monto FROM Multas m INNER JOIN TipoInfracciones ti on m.IdTipoInfraccion=ti.IdTipoInfraccion WHERE ti.Descripcion='No respetar señal de stop' ORDER BY m.Monto desc--33500 Monto de infraccion max
+
 -- 3
 -- Los apellidos y nombres de los agentes que no hayan labrado multas en los dos primeros meses de 2023.
+
+SELECT a.Apellidos, a.Nombres FROM Agentes a where a.IdAgente not in (SELECT distinct a.IdAgente FROM Agentes a INNER JOIN Multas m on a.IdAgente=m.IdAgente WHERE year(m.FechaHora)=2023 and MONTH(m.FechaHora)=1 or year(m.FechaHora)=2023 and MONTH(m.FechaHora)=2)
+
+SELECT distinct a.Apellidos, a.Nombres FROM Agentes a INNER JOIN Multas m on a.IdAgente=m.IdAgente WHERE year(m.FechaHora)=2023 and MONTH(m.FechaHora)=1 or year(m.FechaHora)=2023 and MONTH(m.FechaHora)=2 
+--Agentes que labraron las multas
+
 -- 4
 -- Los apellidos y nombres de los agentes que no hayan labrado multas por 'Exceso de velocidad'.
+
+SELECT a.Apellidos, a.Nombres FROM Agentes a where a.IdAgente not in (SELECT distinct a.IdAgente FROM Agentes a INNER JOIN Multas m on a.IdAgente=m.IdAgente INNER JOIN TipoInfracciones ti on m.IdTipoInfraccion=ti.IdTipoInfraccion WHERE ti.Descripcion='Exceso de velocidad')
+
+SELECT distinct a.IdAgente FROM Agentes a INNER JOIN Multas m on a.IdAgente=m.IdAgente INNER JOIN TipoInfracciones ti on m.IdTipoInfraccion=ti.IdTipoInfraccion WHERE ti.Descripcion='Exceso de velocidad'
+
+
 -- 5
 -- Los legajos, apellidos y nombre de los agentes que hayan labrado multas de todos los tipos de infracciones existentes.
+
+SELECT a.Legajo, a.Apellidos, a.Nombres from Multas m INNER JOIN TipoInfracciones ti on m.IdTipoInfraccion=ti.IdTipoInfraccion INNER JOIN Agentes a on a.IdAgente=m.IdAgente GROUP BY a.Legajo, a.Apellidos, a.Nombres HAVING COUNT(distinct ti.IdTipoInfraccion)=(SELECT COUNT(distinct ti.IdTipoInfraccion) FROM TipoInfracciones ti)
+
+SELECT COUNT(distinct ti.IdTipoInfraccion) FROM TipoInfracciones ti -- Cant de Infracciones.
+
 -- 6
 -- Los legajos, apellidos y nombres de los agentes que hayan labrado más cantidad de multas que la cantidad de multas generadas por un radar (multas con IDAgente con valor NULL)
+
+
+SELECT COUNT(IdMulta) FROM Multas WHERE IdAgente IS NULL --- Cant de multas hechas por RADAR
+
 -- 7
 -- Por cada agente, listar legajo, apellidos, nombres, cantidad de multas realizadas durante el día y cantidad de multas realizadas durante la noche.
 
